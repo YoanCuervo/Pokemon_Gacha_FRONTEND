173acc980eeaa4af58be697c32c031f009ef1eab
YoanCuervo <cuervo.yoan@gmail.com>
2026-07-21 16:24:35 +0200

    feat(inventory): routing in APP for css edit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CombatPage } from "./pages/combat/CombatPage";
 import EditProfil from "./pages/EditProfil";
 import FlagPicker from "./pages/FlagPicker";
 import Home from "./pages/Home";
+import { InventoryPage } from "./pages/inventory/InventoryPage";
 import Profil from "./pages/Profil";
 import Team from "./pages/Team";
 
@@ -17,6 +18,7 @@ function App() {
 					<Route path="/" element={null} />
 					<Route path="/team" element={<Team />} />
 					<Route path="/combat" element={<CombatPage />} />
+					<Route path="/inventory/:instanceId" element={<InventoryPage />} />
 					<Route path="/profil" element={<Profil />}>
 						<Route path="edit" element={<EditProfil />}>
 							<Route path="flag" element={<FlagPicker />} />
